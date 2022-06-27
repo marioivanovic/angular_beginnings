67277cfdfa0ae5e41b6ca8fd179b891f93db8b0d
mrmarioi <ivanovic.mario@gmail.com>
2022-06-27 11:07:45 +0200

    :construction: new modules :construction:

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiModule } from '../ui/ui.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { LoginModule } from '../login/login.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [UiModule, IconsModule, TemplatesModule, LoginModule],
+})
+export class CoreModule { }
